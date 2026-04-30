432473e17a1b68c461b85b5c32e447b2111462ce
ELamraniG <moha.mati@hotmail.com>
2026-04-30 12:13:11 +0100

    fix ticket photo delete

--- backend/src/main/java/com/syndicare/services/TicketsService.java
+++ backend/src/main/java/com/syndicare/services/TicketsService.java
@@ -97,6 +97,7 @@ public class TicketsService {
 
     public void ticketsDeleteService(Long id) {
         Ticket ticket = getTicketById(id);
+        if (ticket.getPhotoPath() != null) fileStorageService.delete(ticket.getPhotoPath());
         ticketRepository.deleteById(id);
     }
 }
